ac7a8601f01f7416cf60b30facff66b27a9646cb
Николай Губин <nikolaygubin@users.noreply.github.com>
2022-03-10 11:21:06 +0700

    read a string str and write function init_figure to read a figure(circle, triangle)

--- main.c
+++ main.c
@@ -1,70 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#define N 100
+
+void init_figure(char *str, char *figure)
+{
+    for (int i = 0; str[i] != '('; i++) {
+        figure[i] = str[i];
+    }
+}
 
 int main()
 {
     FILE* file = fopen("output.txt", "r");
-    int a = 0, b = 0;
-    int i = 0, k = 0, N = 100, number_of_figures;
-    char *str = NULL, cir[] = {"circle"}, tri[] = {"triangle"}, numbers[N];
+    int i = 0, k = 0, number_of_figures;
+    char *str = NULL, *figure =  NULL, cir[7] = {"circle"}, tri[9] = {"triangle"}, numbers[N];
 
     printf("Enter the number figures : ");
     scanf("%d", &number_of_figures);
 
     for (i = 0; i < number_of_figures; i++) {
         str = malloc(N * sizeof(char));
+        figure = malloc(N * sizeof(char));
+
         fgets(str, 100, file);
+        init_figure(str, figure);
 
-        if (strstr(str, cir) != NULL) {
-            for (int p = 0; p < N; p++) {
-                if (str[p] == 41) {
-                    break;
-                }
-                if ((str[p] >= 48 && str[p] <= 57) || str[p] == 44 || str[p] == 46 || str[p] == 32 || str[p] == 45) {
-                    k++;
-                    numbers[k] = str[p];
-                    if (a == 0) {
-                        printf("circle(");
-                        a++;
-                    }
-                    printf("%c", numbers[k]);
-                }
-            }
-            a = 0;
-            if (b == 0) {
-                printf(")");
-                b++;
-            }
-            printf("\n");
-            b = 0;
-        } else if (strstr(str, tri) != NULL) {
-            for (int p = 0; p < N - 1; p++) {
-                if (str[p] == 41) {
-                    break;
-                }
-                if ((str[p] >= 48 && str[p] <= 57) || str[p] == 44 || str[p] == 46 || str[p] == 32 || str[p] == 45) {
-                    k++;
-                    numbers[k] = str[p];
-                    if (a == 0) {
-                        printf("triangle((");
-                        a++;
-                    }
-                    printf("%c", numbers[k]);
-                }
-            }
-            a = 0;
-            if (b == 0) {
-                printf("))");
-                b++;
-            }
-            printf("\n");
-            b = 0;
-        }
+        free(str);
+        free(figure);
     }
-
-    printf("\n");
-    free(str);
 
     fclose(file);
 
